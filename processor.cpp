b1e216d0133f2b80b4ac56c19ebd0644c0981607
anna <ak9ct@virginia.edu>
2020-04-04 01:39:52 -0400

    Revert "fixed prints"
    This reverts commit db84938a1f33a07c8e00dab0d8f9bcb733d7f8b8.

--- processor.cpp
+++ processor.cpp
@@ -37,7 +37,7 @@ void processor_main_loop(Registers &reg_file, Memory &memory, uint32_t end_pc) {
         // fetch: good
         uint32_t instruction;
         memory.access(reg_file.pc, instruction, 0, 1, 0); 
-        cout << "\nPC: 0x" << std::hex << reg_file.pc << std::dec << "\n";
+        //cout << "\nPC: 0x" << std::hex << reg_file.pc << std::dec << "\n";
         // increment pc
         reg_file.pc += 4;
         
