c23f6e2b639f4e9230982847d6eef85983b31100
anna <ak9ct@virginia.edu>
2020-04-03 22:21:26 -0400

    basic pipe-lining working

--- processor.cpp
+++ processor.cpp
@@ -235,50 +235,9 @@ void processor_main_loop(Registers &reg_file, Memory &memory, uint32_t end_pc) {
 
     cout << "CPI = " << (double)num_cycles/(double)num_instrs << "\n";
     
-    /*
-      Temporary Tests for control signals
-    
-      uint32_t instruction;
-      uint32_t op;
-      uint32_t funct;
-      uint32_t ALU_zero = 0;
-      uint32_t result;
-      ifstream inFile;
 
-      inFile.open("controlhex.txt");
-
-      if(!inFile)
-        {
-          cout << "Unable to open file";
-          exit(1);
-        }
-      string temp;
-      while (std::getline(inFile, temp))
-        {
-          std::stringstream ss(temp);
-          ss >> hex >> instruction;
-          cout << "----Instruction: " << temp << "----" << endl;
-          control.decode(instruction); //decodes instruction
-          //control.print(); //prints control signals
-
-
-          op = instruction >> 26;
-          cout<<"op: "<< op <<endl;
-          funct = instruction << 26;
-          funct = funct >> 26;
-          cout<<"funct: "<< funct <<endl;
-          alu.generate_control_inputs(control.ALU_op, funct, op);
-          result = alu.execute(1, 1, ALU_zero);
-
-          alu.print();
-          cout << "ALU_zero: " << ALU_zero << endl;
-          cout << "Result of ALU operation: " << result << endl;
-          cout  << "---------------------------" << endl;
-          ALU_zero = 0;
-      }
-      inFile.close();
-      */
 };
+
 void processor_main_loop_pipeline(Registers &reg_file, Memory &memory, uint32_t end_pc) { //NEED TO FIX HOW THIS WORKS!
 
     // Initialize ALU
@@ -294,41 +253,92 @@ void processor_main_loop_pipeline(Registers &reg_file, Memory &memory, uint32_t 
                         .ALU_src = 0,
                         .reg_write = 0,
                         .store_reg = 0,
-                        .sign_zero = 0};
+                        .sign_zero = 0
+                        };
     
     uint32_t num_cycles = 0;
     uint32_t num_instrs = 0; 
-    state_t current_state;
-    state_t next_state;
-    while (reg_file.pc != end_pc) {
-
-        // fetch: good
-        uint32_t instruction;
-        memory.access(reg_file.pc, instruction, 0, 1, 0); 
-        //cout << "\nPC: 0x" << std::hex << reg_file.pc << std::dec << "\n";
+    state_t current_state; /*={
+      .pc = (uint32_t)0,
+      .ifid = {
+        .instruction = (uint32_t)0,
+        .pc = (uint32_t)0},
+      .idex = {
+        .control = control,
+        .data_reg_1 = (uint32_t)0,
+        .data_reg_2 = (uint32_t)0,
+        .imm = (uint32_t)0,
+        .write_reg = 0},
+      .exmem = {
+        .control = control,
+        .alu_result = (uint32_t)0,
+        .mem_read_data = (uint32_t)0,
+        .mem_write_data = (uint32_t)0,
+        .write_reg = 0},
+      .memwb = {
+        .control = control,
+        .alu_result = (uint32_t)0,
+        .write_reg = 0}
+    }; */
+    state_t next_state; /*={
+      .pc = (uint32_t)0,
+      .ifid = {
+        .instruction = (uint32_t)0,
+        .pc = (uint32_t)0},
+      .idex = {
+        .control = control,
+        .data_reg_1 = (uint32_t)0,
+        .data_reg_2 = (uint32_t)0,
+        .imm = (uint32_t)0,
+        .write_reg = 0},
+      .exmem = {
+        .control = control,
+        .alu_result = (uint32_t)0,
+        .mem_read_data = (uint32_t)0,
+        .mem_write_data = (uint32_t)0,
+        .write_reg = 0},
+      .memwb = {
+        .control = control,
+        .alu_result = (uint32_t)0,
+        .write_reg = 0}
+    }; */
+    int n = 0;
+    while (current_state.pc != end_pc) {
+      cout<<"\n------n: "<<n<<endl;
+      //cout<<"current state pc B: "<<current_state.pc<<endl;
+      //cout<<"current state ifid in: "<<current_state.ifid.instruction<<endl;
+      cout<<"current state pc_write in: "<<current_state.pc_write<<endl;
+      if (current_state.pc_write == 1){//(current_state.ifid.instruction == 0){ //check for fetch stage? (or use pc write)
+        cout<<"fetch"<<endl;
+        //fetch
+        memory.access(current_state.pc, current_state.ifid.instruction, 0, 1, 0);  //from the current state's pc, grab the insturction
         // increment pc
-        reg_file.pc += 4;
-        
-        // TODO: fill in the function argument
-        // decode into contol signals
+        cout << "\nPC: 0x" << std::hex << current_state.pc << std::dec << "\n";
+        cout<< "Instruction: " << current_state.ifid.instruction <<endl;
+        next_state.ifid.instruction = current_state.ifid.instruction; //set next stages's ifid instruction value (since we will be going to that stage next)
+        next_state.pc += 4; //set up for next instruction fetch
+        num_instrs++; 
+        cout<<"# in: "<< num_instrs<<endl;
+        next_state.ifid.ifid_write = 1; //next cycle do next stage (id)
+      }
+
+      //id stage
+      if(current_state.ifid.ifid_write == 1){ //if this is id stage time
+      cout<<"decode"<<endl;
+        uint32_t instruction = current_state.ifid.instruction;
         control.decode(instruction);
-        //cout<< "Instruction: " << instruction <<endl;
-        //control.print(); // used for autograding 
-        
-        // TODO: fill in the function argument
-        // Read from reg file
-        
+        next_state.idex.control = control; //set the next state's control inputs 
         /****get rs: good****/
         uint32_t rs_b = instruction << 6; //get rid of opcode
         rs_b = rs_b >> 27; //get rs
         int32_t rs_num = (int32_t) rs_b; //convert rs to int
-        //cout<< "RS_num: " << rs_num <<endl; // prints rs value
+        next_state.idex.rs = rs_num; //set the rs reg # in the next state
         
         /****get rt: good****/
         uint32_t rt_b = instruction << 11; //get rid of opcode and rs
         rt_b = rt_b >>27; //get rt
         int rt_num = (int32_t) rt_b; //convert rt to int
-        //cout<<"Rt_num: "<<rt_num<<endl; // prints rt value
+        next_state.idex.rt = rt_num; //set the rs reg # in the next state
 
         uint32_t data_rs; //variable for data of rs
         uint32_t data_rt; //varaible for data of rt
@@ -340,27 +350,24 @@ void processor_main_loop_pipeline(Registers &reg_file, Memory &memory, uint32_t 
         /****get opcode & funct: good ****/
         uint32_t op = instruction >> 26; // gets op
         //cout << "op: "<< op <<endl; //prints opcode
+        next_state.idex.op - op;
         uint32_t funct = instruction << 26; // gets funct (see next too)
         funct = funct >>26;
+        next_state.idex.funct = funct;
         //cout<<"funct: "<<funct<<endl; //prints funct
 
         if (op == 0){ //if r-type
             uint32_t rd_b = instruction << 16; //get rid of op, rs, rt
             rd_b = rd_b >> 27; // isolate rd
             rd_num = (int32_t) rd_b; //convert rd to int
-            //cout<< "Rd_num: " << rd_num <<endl; //prints out rd
-
             reg_file.access(rs_num, rt_num, data_rs, data_rt, rd_num, 0, data_write); // gets rs, rt values
-            //cout<<"rs_data: "<< data_rs <<endl;
-            //cout<<"rt_data: "<< data_rt <<endl;
-
             if(funct == 2 || funct == 0) // checks to see if it's either shifts
             {
               uint32_t shamt = rd_b << 5;
               shamt = shamt >> 27; //isolate shamt
-              //cout<< "shamt: " << shamt <<endl; //prints out shamt
+              next_state.idex.shamt = shamt; //set the shamt varaible 
             }
-            else if(funct == 8) // checks to see if it's jumpReg
+            else if(funct == 8) // checks to see if it's jumpReg - NOT WORK RIGHT NOW!!
             {
               reg_file.pc = reg_file.pc - 4 + data_rs; // PC=R[rs]
             }
@@ -370,45 +377,55 @@ void processor_main_loop_pipeline(Registers &reg_file, Memory &memory, uint32_t 
 
           data_i = instruction << 16; //gets immediate vales
           data_i = data_i >> 16;
+           next_state.idex.imm = data_i; //put in the imm for next state
 
           if(op == 0b001100 || op == 0b001101) //logic operations - andi ori 
           {
             reg_file.access(rs_num, 0, data_rs, data_rt, rt_num, 0, data_write);
             data_i = data_i & 0x0000ffff; // zero extends first 16 bits
+            next_state.idex.imm = data_i; //put in the imm for next state
           }
           else if(op == 0b001111) // lui
           {
             data_i = data_i << 16;
             data_i = data_i & 0xffff0000;
+             next_state.idex.imm = data_i; //put in the imm for next state
           }
           else if(op == 0b101000 || op == 0b101001 || op == 0b101011) // sb, sh, sw
           {
             reg_file.access(rs_num, rt_num, data_rs, data_rt, 0, 0, data_write); //acess reg_file to get the data of rt
+            next_state.idex.write_data = data_rt; //put in value for next state
             if(control.store_reg == 0b01) // sb
             {
               data_rt = data_rt & 0x000000ff; // only takes lower 8 bits
+              next_state.idex.write_data = data_rt; //put in value for next state
             }
             else if(control.store_reg == 0b00) // sh
             {
               data_rt = data_rt & 0x0000ffff; // only takes lower 16 bits
+              next_state.idex.write_data = data_rt; //put in value for next state
             }  
            // cout << "rt_data for stores: " << data_rt << endl;       
           }
           else // rest of regular arithmetic and and loads
           {
             reg_file.access(rs_num, 0, data_rs, data_rt, rt_num, 0, data_write);
+            
           }
-          
-         // cout<< "data_i: " << data_i <<endl; //prints immediate value 
-          //cout<< "rs_data: "<< data_rs <<endl;
-          //cout<<"rt_data: "<<data_rt<<endl;
-        }
-      
-        // TODO: fill in the function argument
-        // Execution 
-        alu.generate_control_inputs(control.ALU_op, funct, op);
-        //alu.print();
-
+          next_state.idex.idex_write = 1; //go to next stage
+        
+       }
+         next_state.idex.print();
+      }
+      //execution
+      if(current_state.idex.idex_write == 1){
+        cout<<"execution"<<endl;
+        uint32_t funct = current_state.idex.funct;
+        uint32_t op = current_state.idex.op;
+        uint32_t data_rs = current_state.idex.data_rs;
+        uint32_t data_rt = current_state.idex.data_rt;
+        uint32_t shamt = current_state.idex.shamt;
+        alu.generate_control_inputs(current_state.idex.control.ALU_op, current_state.idex.funct, current_state.idex.op);
         uint32_t alu_zero;
         uint32_t alu_result;
 
@@ -425,81 +442,116 @@ void processor_main_loop_pipeline(Registers &reg_file, Memory &memory, uint32_t 
         }
         else //if itype
         {
-          alu_result = alu.execute(data_rs, data_i, alu_zero);
+          alu_result = alu.execute(data_rs, current_state.idex.imm, alu_zero);
         }
 
-        //int32_t r = (int32_t) alu_result;
-        //cout<< "alu result: " << r << endl;
-        data_write = alu_result;
+        //data_write = alu_result;
+        next_state.exmem.control = current_state.idex.control; //copy controls
+        next_state.exmem.alu_result = alu_result; //put the data that needs to be written in alu_result
+        next_state.exmem.write_data = current_state.idex.write_data; //put the data that needs to be written
+        next_state.exmem.rt = current_state.idex.rt; //copy registers
+        next_state.exmem.rs = current_state.idex.rs; //copy registers
+        next_state.exmem.rd = current_state.idex.rd; //copy registers
+        next_state.exmem.data_rs = current_state.idex.data_rs; //copy data of registers for reg access
+        next_state.exmem.data_rt = current_state.idex.data_rt; //copy data of registers for reg access
+        next_state.exmem.exmem_write = 1; //do next stage next cycle
+      }
 
-        
-        uint32_t mem_data;
-        // Memory
-        // TODO: fill in the function argument
-        if(control.mem_read == 1 || control.mem_write == 1)
+
+      //memory
+      if(current_state.exmem.exmem_write == 1){
+        cout<<"memory"<<endl;
+        uint32_t alu_result = current_state.exmem.alu_result;
+        uint32_t data_rs = current_state.exmem.data_rs;
+        uint32_t data_rt = current_state.exmem.data_rt;
+
+        next_state.memwb.data_rs = data_rs; //put the value for rs into the reg
+         if(current_state.exmem.control.mem_read == 1 || current_state.exmem.control.mem_write == 1)
         {
-          if(control.store_reg > 2) //stores
+          if(current_state.exmem.control.store_reg > 2) //stores
           {
-            if(control.store_reg == 1) // sb
+            if(current_state.exmem.control.store_reg == 1) // sb
             {
               memory.access(alu_result, data_rs, data_rt, 1, 0); // take value from memory
               data_rs = data_rs & 0xffffff00; // get rid of rightmost 8 bits
               data_rs = data_rs & data_rt; // replace rightmost 8 bits with rt
-              memory.access(alu_result, data_rs, data_rs, control.mem_read, control.mem_write); // write modified value to memory
+              memory.access(alu_result, data_rs, data_rs, current_state.exmem.control.mem_read, current_state.exmem.control.mem_write); // write modified value to memory
             }
-            if(control.store_reg == 0) // sh
+            if(current_state.exmem.control.store_reg == 0) // sh
             { 
               memory.access(alu_result, data_rs, data_rt, 1, 0); // take value from memory
               data_rs = data_rs & 0xffff0000; // get rid of rightmost 16 bits
               data_rs = data_rs & data_rt; // replace rightmost 16 bits with rt
-              memory.access(alu_result, data_rs, data_rs, control.mem_read, control.mem_write); // write modified value to memory
+              memory.access(alu_result, data_rs, data_rs, current_state.exmem.control.mem_read, current_state.exmem.control.mem_write); // write modified value to memory
             }
+            next_state.memwb.data_rs = data_rs; //put the value for rs into the reg
           }
-          else if(control.mem_to_reg > 0) //loads
+          else if(current_state.exmem.control.mem_to_reg > 0) //loads
           {
-            memory.access(alu_result, data_rs, data_rt, control.mem_read, control.mem_write); // regular load word
-            if(control.mem_to_reg == 0b10) // lbu
+            memory.access(alu_result, data_rs, data_rt, current_state.exmem.control.mem_read, current_state.exmem.control.mem_write); // regular load word
+            if(current_state.exmem.control.mem_to_reg == 0b10) // lbu
             {
               data_rs = data_rs & 0x000000ff;
+              next_state.memwb.data_rs = data_rs; //put the value for rs into the reg
             }
-            if(control.mem_to_reg == 0b11) // lhu
+            if(current_state.exmem.control.mem_to_reg == 0b11) // lhu
             {
               data_rs = data_rs & 0x0000ffff;
+              next_state.memwb.data_rs = data_rs; //put the value for rs into the reg
             }
           }
           else // regular store word
           {
-            memory.access(alu_result, data_rs, data_rt, control.mem_read, control.mem_write);
+            memory.access(alu_result, data_rs, data_rt, current_state.exmem.control.mem_read, current_state.exmem.control.mem_write);
+            next_state.memwb.data_rs = data_rs; //put the value for rs into the reg
           }
         }
+        next_state.memwb.control = current_state.exmem.control; //copy controls
+        next_state.memwb.alu_result = alu_result; //put the data that needs to be written in alu_result
+        next_state.memwb.write_data = current_state.exmem.write_data; //put the data that needs to be written
+        next_state.memwb.rt = current_state.exmem.rt; //copy registers
+        next_state.memwb.rs = current_state.exmem.rs; //copy registers
+        next_state.memwb.rd = current_state.exmem.rd; //copy registers
+        next_state.memwb.data_rt = current_state.exmem.data_rt; //copy data of registers for reg access
+        next_state.memwb.memwb_write = 1; //do next stage next cycle
+      }
 
-        
-        // Write Back
-        // TODO: fill in the function argument
-        if (control.reg_write == 1) // might need to fix
-        {
-          if(control.reg_dest == 0) // write to rt
-          { //write to rt
+      //write back
+       if(current_state.memwb.memwb_write == 1){ //if we should write to this stage
+       cout<<"write back"<<endl;
+        uint32_t data_rs = current_state.memwb.data_rs;
+        uint32_t data_rt = current_state.memwb.data_rt;
+        uint32_t rt_num = current_state.memwb.rt;
+        uint32_t rs_num = current_state.memwb.rs;
+        uint32_t rd_num = current_state.memwb.rd;
+        uint32_t data_write = current_state.memwb.write_data;
+        if (current_state.memwb.control.reg_write == 1) {// might need to fix
+          if(current_state.memwb.control.reg_dest == 0){ //write to rt
             //cout<<"write to rt"<<endl;
             reg_file.access(rs_num, 0, data_rs, data_rs, rt_num, 1, data_write);
           }
-          else if(control.reg_dest == 1) //write to rd
-          { 
+          else if(current_state.memwb.control.reg_dest == 1) { //write to rd
             //cout<< "write to rd" <<endl;
             reg_file.access(rs_num, rt_num, data_rs, data_rt, rd_num, 1, data_write);
           }
         }
+        //set singal?
+       }
+        /*
         
-        
-        // TODO: Update PC
+      // TODO: Update PC
 
 
         //cout << "CYCLE" << num_cycles << "\n";
         reg_file.print(); // used for automated testing
-
+ */
         num_cycles++;
-        num_instrs++; 
-        cout<<"# in: "<< num_instrs<<endl;
+        //cout<<"current state pc: "<<current_state.pc<<endl;
+        //cout<<"next state pc: "<<next_state.pc<<endl;
+        current_state = next_state;
+        //cout<<"next state pc2: "<<next_state.pc<<endl;
+        //cout<<"current state pc2: "<<current_state.pc<<endl;
+        n++;
 
     }
 
--- state.h
+++ state.h
@@ -12,6 +12,7 @@
 struct IFID {
     uint32_t instruction;
     uint32_t pc; //pc+1?
+    bool ifid_write = 0; //write to ifid if 1; 0 do not
 };
 
 // TODO:
@@ -19,10 +20,33 @@ struct IFID {
 //needs the read register data and extention of the imm to send to alu
 struct IDEX {
     control_t control;
-    uint32_t data_reg_1; //reg 1 data 
-    uint32_t data_reg_2; // reg 2 data
+    uint32_t data_rs; //reg 1 data 
+    uint32_t data_rt; // reg 2 data
+    int rs; //rs register for hazard
+    int rt; //rt register
+    int rd; //rd register for hazard
     uint32_t imm; //32 bit extended imm
+    int shamt; //for shifts
+    uint32_t write_data; // for things like stores 
+    uint32_t funct; //for alu execute
+    uint32_t op; //for alu execute
     int write_reg; //for reg write
+    bool idex_write = 0; //bool writing to idex = 1, not write if 0
+    void print(){
+        cout<<"control: "<<endl;
+        control.print();
+        cout<<"data_rs: "<<data_rs<<endl;
+        cout<<"data_rt: "<<data_rt<<endl;
+        cout<<"rs: "<<rs<<endl;
+        cout<<"rt: "<<rt<<endl;
+        cout<<"rd: "<<rd<<endl;
+        cout<<"imm: "<<imm<<endl;
+        cout<<"shamt: "<<shamt<<endl;
+        cout<<"write_data: "<<write_data<<endl;
+        cout<<"funct: "<<funct<<endl;
+        cout<<"op: "<<op<<endl;
+        cout<<"write_reg: "<<write_reg<<endl;
+    }
 };
 
 // TODO:
@@ -31,9 +55,14 @@ struct IDEX {
 struct EXMEM {
     control_t control;
     uint32_t alu_result; //take the result of ALU to use
-    uint32_t mem_read_data; //the data that should be read into memory
-    uint32_t mem_write_data; //the data that should be written into memory
+    uint32_t write_data; //the data that should be written into memory
+    int rs; //rs register for hazard
+    int rd; //rd register for hazard
+    int rt; //rt register
+    uint32_t data_rs; //reg 1 data 
+    uint32_t data_rt; // reg 2 data
     int write_reg; //for reg write
+    bool exmem_write = 0; //bool writing to idex = 1, not write if 0
 };
 
 // TODO:
@@ -42,17 +71,25 @@ struct EXMEM {
 struct MEMWB {
     control_t control;
     uint32_t alu_result; //data to write to reg from alu??
-    int write_reg; //for reg write
+    int rs; //rs register for hazard
+    int rd; //rd register for hazard
+    int rt; //rt register
+    uint32_t data_rs; //reg 1 data 
+    uint32_t data_rt; // reg 2 data
+    uint32_t write_data; //for data for write
+    bool memwb_write = 0; //bool writing to idex = 1, not write if 0
 
 };
 
 struct state_t {
+    //string state; //name of the state currently in
     uint32_t pc;
     IFID ifid;
     IDEX idex;
     EXMEM exmem;
     MEMWB memwb;
-};
-
-
+    bool pc_write = 1; //1 to write to pc (fetch), 0 do not fetch
+    
+};        
+        
 #endif
